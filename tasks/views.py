bcc6ab00a3464da84d943257ede5d9bee1676074
Tanvir-yzu <2020tanvir1971@gmail.com>
2025-01-15 00:34:28 +0800

    sp

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello from the tasks app!")
 
+
 def manager_dashboard(request):
     return render(request, 'admin-dashboard.html')
+
+
 def user_dashboard(request):
     return render(request, 'user-dashboard.html')
+
 
 def about(request):
     return render(request, 'about.html')
